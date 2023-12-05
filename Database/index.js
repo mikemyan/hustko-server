8bc587c2446a7b227fb4682d69d3aaf0af7ecfdb
mikemyan <mikeyan1997@gmail.com>
2023-12-05 16:25:43 -0500

    mongo connected

---
+++ Database/index.js
@@ -0,0 +1,2 @@
+import users from "./users.json" assert { type: "json" };
+export default { users };
--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+mongoose.connect(
+  "mongodb+srv://hustkoAdmin:ES1SuPktduCOikna@hustko.b7jibdd.mongodb.net/?retryWrites=true&w=majority",
+  {}
+);
 const app = express();
+app.use(cors());
+app.use(express.json());
 app.listen(4000);
