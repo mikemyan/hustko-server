bf01790f78a83c06c4f2d2ef1fa632b3e8d5a730
mikemyan <mikeyan1997@gmail.com>
2023-12-07 10:58:04 -0500

    added more products apis

--- DAO/ProductsDAO.js
+++ DAO/ProductsDAO.js
@@ -1,4 +1,13 @@
 import ProductsModel from "../Models/ProductsModel.js";
 
 export const findProductByNameDAO = (name) =>
-    ProductsModel.find({ name: { $regex: name, $options: "i" } });
+  ProductsModel.find({ name: { $regex: name, $options: "i" } });
+export const findAllProducts = () => ProductsModel.find();
+export const findProductById = (productId) => ProductsModel.findById(productId);
+export const createProduct = (product) => ProductsModel.create(product);
+export const updateProduct = (productId, product) => {
+  ProductsModel.updateOne({ _id: productId }, { $set: product });
+};
+export const deleteProduct = (productId) => {
+  ProductsModel.deleteOne({ _id: productId });
+};
