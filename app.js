a9c745e5460b935d52f2f696a6da7314d68672a7
mikemyan <mikeyan1997@gmail.com>
2023-12-06 21:33:55 -0500

    user dao

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 import mongoose from "mongoose";
-mongoose.connect(
-  "mongodb+srv://hustkoAdmin:ES1SuPktduCOikna@hustko.b7jibdd.mongodb.net/?retryWrites=true&w=majority",
-  {}
-);
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+// const CONNECTION_STRING =
+//   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+const CONNECTION_STRING =
+  "mongodb+srv://hustkoAdmin:ES1SuPktduCOikna@hustko.b7jibdd.mongodb.net/hustko?retryWrites=true&w=majority";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
-app.listen(4000);
+UserRoutes(app);
+app.listen(process.env.PORT || 4000);
---
+++ users/schema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: String,
+    lastName: String,
+    dob: Date,
+    role: {
+      type: String,
+      enum: ["CUSTOMER", "RETAILER", "ADMIN"],
+      default: "CUSTOMER",
+    },
+    mobile: Number,
+    primAddress: String,
+  },
+  { collection: "users" }
+);
+export default userSchema;
